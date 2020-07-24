ae7e9933d14494d1d0bb336895b8c273d90adc18
Albert <albertlai631@outlook.com>
2020-07-24 15:54:01 -0400

    feat: onCreate implementation

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.onNewAnnouncement = functions.database
+  .ref("announcements")
+  .onCreate((snapshot, _context) => {
+    const messageData = snapshot.val();
+    const text = messageData.join(" ");
+    console.log(text);
+
+    var topic = "announcements";
+
+    //admin.messaging().sendToTopic(topic);
+  });
