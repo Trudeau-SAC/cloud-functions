f669f60356de317d86ce6490b1a0c0113f3da15b
Albert <albertlai631@outlook.com>
2020-07-24 17:34:19 -0400

    feat: initial function finish

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,25 @@ exports.onNewAnnouncement = functions.database
     console.log(text);
 
     var topic = "announcements";
+    const payload = {
+      notification: {
+        title: "Today's Announcements",
+        body: text,
+        sound: "default",
+      },
+      data: {
+        clickAction: "FLUTTER_NOTIFICATION_CLICK",
+      },
+    };
 
-    //admin.messaging().sendToTopic(topic);
+    admin
+      .messaging()
+      .sendToTopic(topic, payload)
+      // eslint-disable-next-line promise/always-return
+      .then((response) => {
+        console.log("Successfully sent message:", response);
+      })
+      .catch((error) => {
+        console.log("Error sending message:", error);
+      });
   });
