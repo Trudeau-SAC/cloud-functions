68aa47a9633b8b78ba2f645c337a691082b25817
Albert <albertlai631@outlook.com>
2021-04-19 14:06:20 -0400

    docs: comment index.js

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,22 @@
+// Cloud Functions for Firebase SDK
 const functions = require("firebase-functions");
 
+// Firebase Admin SDK to access RTDB
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+// redeploy all functions: firebase deploy --only functions
+
+// Trigger for new entry in announcements index in RTDB
+// deploy: firebase deploy --only functions:onNewAnnouncement
 exports.onNewAnnouncement = functions.database
   .ref("/announcements/{date}")
   .onCreate((snapshot, _context) => {
+    // store child value
     const messageData = snapshot.val();
     const date = snapshot.key;
     var topic = "announcements";
+    // declare payload
     const payload = {
       notification: {
         title: "New Announcements for " + date.substr(date.indexOf(" ") + 1),
@@ -22,6 +30,7 @@ exports.onNewAnnouncement = functions.database
       },
     };
 
+    //send notification
     return admin
       .messaging()
       .sendToTopic(topic, payload)
@@ -33,13 +42,14 @@ exports.onNewAnnouncement = functions.database
       });
   });
 
-//firebase deploy --only functions
 //firebase deploy --only functions:oneNewNotification
 exports.oneNewNotification = functions.database
   .ref("/notifications/{index}")
   .onCreate((snapshot, _context) => {
+    // store child value
     const messageData = snapshot.val();
     var topic = messageData["topic"];
+    // declare payload
     const payload = {
       notification: {
         body: messageData["message"],
@@ -52,14 +62,17 @@ exports.oneNewNotification = functions.database
       },
     };
 
+    // if the screen isn't N/A, declare the screen
     if (messageData["screen"] !== "N/A") {
       payload["data"]["screen"] = messageData["screen"];
     }
 
+    // if the title isn't N/A, declare the title
     if (messageData["title"] !== "N/A") {
       payload["notification"]["title"] = messageData["title"];
     }
 
+    //send notification
     return admin
       .messaging()
       .sendToTopic(topic, payload)
@@ -75,11 +88,13 @@ exports.oneNewNotification = functions.database
 exports.onNewClub = functions.database
   .ref("/clubs/{name}")
   .onCreate((snapshot, _context) => {
+    // store child value
     const messageData = snapshot.val();
     const name = snapshot.key;
-
+    //check if there is a club of the week
     if (name !== "None") {
       var topic = "clubs";
+      // declare payload
       const payload = {
         notification: {
           title: "Club of the Week: " + name,
@@ -93,6 +108,7 @@ exports.onNewClub = functions.database
         },
       };
 
+      //send notification
       return admin
         .messaging()
         .sendToTopic(topic, payload)
@@ -103,5 +119,7 @@ exports.onNewClub = functions.database
           return console.log("Error sending message:", error);
         });
     }
+
+    // if no club of the week, don't send a notification
     return console.log("No Club of the Week");
   });
